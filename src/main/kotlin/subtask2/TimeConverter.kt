368389689b9a75a4253fa1c68cc9b2e161ac4b18
Kozel_Pavel <pavelshherba@gmail.com>
2020-04-12 15:17:42 +0300

    feat: realize subtask2 - TimeConverter

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -2,8 +2,32 @@ package subtask2
 
 class TimeConverter {
 
-    // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
+        val minute = minute.toInt()
+        val hour = hour.toInt()
+
+        val numeralWords = arrayOf(
+            "", "one", "two", "three", "four", "five",
+            "six", "seven", "eight", "nine", "then",
+            "eleven", "twelve", "thirteen", "fourteen",
+            "fifteen", "sixteen", "seventeen", "eighteen",
+            "nineteen", "twenty", "twenty one", "twenty two",
+            "twenty three", "twenty four", "twenty five",
+            "twenty six", "twenty seven", "twenty eight", "twenty nine"
+        )
+
+        if (minute > 60 || hour > 12) return numeralWords[0]
+
+        return when (minute) {
+            0 -> "${numeralWords[hour]} o' clock"
+            1 -> "${numeralWords[1]} minute past ${numeralWords[hour]}"
+            15 -> "quarter past ${numeralWords[hour]}"
+            30 -> "half past ${numeralWords[hour]}"
+            45 -> "quarter to ${numeralWords[hour + 1]}"
+            in 1 until 30 -> "${numeralWords[minute]} minutes past ${numeralWords[hour]}"
+            in 31..60 -> "${numeralWords[60 - minute]} minutes to ${numeralWords[hour + 1]}"
+
+            else -> numeralWords[0]
+        }
     }
 }
