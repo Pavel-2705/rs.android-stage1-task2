9ca632b65da0f646727abb57ceb36f497ae75755
Kozel_Pavel <pavelshherba@gmail.com>
2020-04-12 21:22:53 +0300

    fix: fix mistake in while cycle.

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -6,7 +6,7 @@ class Fibonacci {
         var f1 = 0
         var f2 = 1
 
-        while (f1 * f2 <= n) {
+        while (f1 * f2 < n) {
             val temp = f2
             f2 += f1
             f1 = temp
