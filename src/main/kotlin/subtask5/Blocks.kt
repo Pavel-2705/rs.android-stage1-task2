8fa333285c628052162122c46ea9703b332e2c5d
Kozel_Pavel <pavelshherba@gmail.com>
2020-04-12 21:14:15 +0300

    fix: add setLocale("ru")

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -2,18 +2,20 @@ package subtask5
 
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.*
 import kotlin.reflect.KClass
 
 class Blocks {
 
     fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
+        val locale = Locale("ru")
         val dateFormat = "dd.MM.yyyy"
 
         return when (blockB) {
             Int::class -> blockA.filterIsInstance<Int>().sum()
             String::class -> blockA.filterIsInstance<String>().joinToString("")
             else -> blockA.filterIsInstance<LocalDate>().max()!!.format(
-                DateTimeFormatter.ofPattern(dateFormat)
+                DateTimeFormatter.ofPattern(dateFormat, locale)
             )
         }
 
