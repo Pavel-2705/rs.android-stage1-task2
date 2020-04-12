fe1aadaa9a7f80076b740beab1b566b8abb66ac8
Kozel_Pavel <pavelshherba@gmail.com>
2020-04-12 19:05:42 +0300

    feat: subtask4 - Pangram

--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -4,6 +4,34 @@ class Pangram {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): String {
-        throw NotImplementedError("Not implemented")
+        val vowels = setOf('a', 'e', 'i', 'o', 'u', 'y')
+        val regexForVowels = "[aeiouy]".toRegex()
+        val regexForConsonants = "[qwrtpsdfghjklzxcvbnm]".toRegex()
+
+        val isPangram = isStringPangram(inputString)
+
+        val mutableListWords =
+            inputString.replace('\n', ' ')
+                .split(' ')
+                .filter { it.isNotEmpty() }
+                    as MutableList
+
+        for (index in mutableListWords.indices) {
+            mutableListWords[index] =
+                if (isPangram) mutableListWords[index].count { char -> vowels.contains(char.toLowerCase()) }.toString() +
+                        regexForVowels.replace(mutableListWords[index]) { vowel -> vowel.value.toUpperCase() }
+                else mutableListWords[index].count { char -> !vowels.contains(char.toLowerCase()) and (char.toLowerCase() in 'a'..'z') }.toString() +
+                        regexForConsonants.replace(mutableListWords[index]) { consonants -> consonants.value.toUpperCase() }
+        }
+
+        mutableListWords.sortWith(compareBy { it[0] })
+
+        return mutableListWords.joinToString(" ")
+    }
+
+    private fun isStringPangram(string: String): Boolean {
+        val variousLetterFromString = mutableSetOf<Char>()
+        variousLetterFromString.addAll(string.toLowerCase().asIterable().filter { it in 'a'..'z' })
+        return variousLetterFromString.size == 26
     }
 }
